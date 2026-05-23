d3eb4abbaf4ec72f56c05fefc09467b9e76857af
Angelos Semoglou <a.semoglou@outlook.com>
2026-05-23 15:20:36 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="extclustval",
+    version="0.1.0",
+    description="A lightweight Python package for external clustering validation metrics.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Aggelos Semoglou",
+    author_email="a.semoglou@outlook.com",
+    license="MIT",
+    url="https://github.com/semoglou/extclustval",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy>=1.21",
+        "scipy>=1.7",
+        "scikit-learn>=1.0",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+)
