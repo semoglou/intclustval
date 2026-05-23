d5c1fbe73b4ebe60779c42ba679da745174d2756
Angelos Semoglou <a.semoglou@outlook.com>
2026-05-23 15:50:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="intclustval",
-    version="0.1.0",
+    version="0.1.2",
     description="A lightweight Python package for internal clustering validation metrics.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
